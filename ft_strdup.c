4f315b97f3f66a0756728a7e2bbd51cfea02fc77
MyNameIsTrez <welfje@gmail.com>
2021-10-28 11:51:21 +0200

    Use stralloc instead of malloc

--- ft_strdup.c
+++ ft_strdup.c
@@ -6,20 +6,23 @@
 /*   By: sbos <sbos@student.codam.nl>                 +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/10/11 12:40:20 by sbos          #+#    #+#                 */
-/*   Updated: 2021/10/19 13:24:01 by sbos          ########   odam.nl         */
+/*   Updated: 2021/10/26 15:46:14 by sbos          ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-#include <stdlib.h>
+static char	*ft_stralloc(size_t n)
+{
+	return (ft_calloc(n + 1, sizeof(char)));
+}
 
 char	*ft_strdup(const char *s1)
 {
 	char	*dup;
 
-	dup = malloc(ft_strlen(s1) + 1);
+	dup = ft_stralloc(ft_strlen(s1));
 	if (dup)
-		ft_memcpy(dup, s1, ft_strlen(s1) + 1);
+		ft_memcpy(dup, s1, ft_strlen(s1));
 	return (dup);
 }
--- ft_strjoin.c
+++ ft_strjoin.c
@@ -6,13 +6,16 @@
 /*   By: sbos <sbos@student.codam.nl>                 +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/10/11 12:40:15 by sbos          #+#    #+#                 */
-/*   Updated: 2021/10/19 16:55:16 by sbos          ########   odam.nl         */
+/*   Updated: 2021/10/26 16:01:51 by sbos          ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <libft.h>
 
-#include <stdlib.h>
+static char	*ft_stralloc(size_t n)
+{
+	return (ft_calloc(n + 1, sizeof(char)));
+}
 
 char	*ft_strjoin(char const *s1, char const *s2)
 {
@@ -24,12 +27,11 @@ char	*ft_strjoin(char const *s1, char const *s2)
 		return (NULL);
 	s1_len = ft_strlen(s1);
 	s2_len = ft_strlen(s2);
-	joined = malloc(s1_len + s2_len + 1);
+	joined = ft_stralloc(s1_len + s2_len);
 	if (joined)
 	{
-		ft_memcpy(joined, s1, s1_len);
-		ft_memcpy(joined + s1_len, s2, s2_len);
-		joined[s1_len + s2_len] = '\0';
+		ft_memcpy(&joined[0], s1, s1_len);
+		ft_memcpy(&joined[s1_len], s2, s2_len);
 	}
 	return (joined);
 }
