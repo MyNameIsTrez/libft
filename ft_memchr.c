c1efdafd7cfc65bbac31412bfc6e6f7760ddb643
MyNameIsTrez <welfje@gmail.com>
2021-11-04 15:28:42 +0100

    Expand memchr documentation

--- ft_memchr.c
+++ ft_memchr.c
@@ -6,31 +6,37 @@
 /*   By: sbos <sbos@student.codam.nl>                 +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/10/11 12:40:46 by sbos          #+#    #+#                 */
-/*   Updated: 2021/11/03 14:32:46 by sbos          ########   odam.nl         */
+/*   Updated: 2021/11/04 15:27:27 by sbos          ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stddef.h>
 
 /**
- * @brief Scans memory for a character.
+ * @brief Scans the initial @p len bytes of the memory area pointed to by @p ptr
+   for the first instance of @p chr.\n
+   Both @p chr and the bytes of the memory area pointed to by @p ptr
+   are interpreted as unsigned char.
  *
  * @param ptr
  * @param chr
  * @param len
- * @return
+ * @return A pointer to the located @p chr;\n
+ * NULL if @p chr isn't in @p len bytes of @p ptr.
  */
 void	*ft_memchr(const void *ptr, int chr, size_t len)
 {
-	size_t	i;
-	char	*chr_ptr;
+	char			*ptr_;
+	unsigned char	chr_;
+	size_t			i;
 
-	chr_ptr = (char *)ptr;
+	ptr_ = (char *)ptr;
+	chr_ = (unsigned char)chr;
 	i = 0;
 	while (i < len)
 	{
-		if (chr_ptr[i] == chr)
-			return ((void *)&chr_ptr[i]);
+		if (ptr_[i] == chr_)
+			return ((void *)&ptr_[i]);
 		i++;
 	}
 	return (NULL);
