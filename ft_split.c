ac09ffb09feb3060005bb562f10fff771ae087ed
MyNameIsTrez <welfje@gmail.com>
2021-11-08 15:37:02 +0100

    Refactor ft_split

--- ft_split.c
+++ ft_split.c
@@ -6,7 +6,7 @@
 /*   By: sbos <sbos@student.codam.nl>                 +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/10/11 12:40:26 by sbos          #+#    #+#                 */
-/*   Updated: 2021/11/04 16:23:04 by sbos          ########   odam.nl         */
+/*   Updated: 2021/11/08 15:36:30 by sbos          ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,23 +31,21 @@ static char	*ft_str_not_chr(const char *str, const int chr)
 		return (NULL);
 }
 
-static size_t	get_word_count(char *str, char chr)
+static size_t	get_word_count(char *str, char sep)
 {
 	size_t	word_count;
 	char	*start_of_word;
 	char	*end_of_word;
 
 	word_count = 0;
-	while (*str != '\0')
+	while (str != NULL && str[0] != '\0')
 	{
-		start_of_word = ft_str_not_chr(str, chr);
+		start_of_word = ft_str_not_chr(str, sep);
 		if (start_of_word == NULL)
 			break ;
-		end_of_word = ft_strchr(start_of_word, chr);
+		end_of_word = ft_strchr(start_of_word, sep);
 		str = end_of_word;
 		word_count++;
-		if (str == NULL)
-			break ;
 	}
 	return (word_count);
 }
@@ -66,27 +64,27 @@ static void	free_split(char ***split)
 	*split = NULL;
 }
 
-static void	add_words_to_split(char *str, char chr, char ***split)
+static void	add_words_to_split(char *str, char sep, char ***split)
 {
-	size_t	i;
-	char	*start_of_word;
-	char	*end_of_word;
-	size_t	len;
+	size_t			i;
+	char			*start_of_word;
+	char			*end_of_word;
+	unsigned int	start_index;
+	size_t			len;
 
 	i = 0;
-	while (*str != '\0')
+	while (str != NULL && str[0] != '\0')
 	{
-		start_of_word = ft_str_not_chr(str, chr);
+		start_of_word = ft_str_not_chr(str, sep);
 		if (start_of_word == NULL)
 			break ;
-		end_of_word = ft_strchr(start_of_word, chr);
-		len = (size_t)(end_of_word - start_of_word);
-		(*split)[i] = ft_substr(str, (unsigned int)(start_of_word - str), len);
+		end_of_word = ft_strchr(start_of_word, sep);
+		start_index = (unsigned int)(&start_of_word[0] - &str[0]);
+		len = (size_t)(&end_of_word[0] - &start_of_word[0]);
+		(*split)[i] = ft_substr(str, start_index, len);
 		if ((*split)[i] == NULL)
 			return (free_split(split));
 		str = end_of_word;
-		if (str == NULL)
-			break ;
 		i++;
 	}
 }
