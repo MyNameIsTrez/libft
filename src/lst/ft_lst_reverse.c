076cd8f83b57d5ea71cbb20afe1642b662976384
MyNameIsTrez <welfje@gmail.com>
2021-11-30 15:13:32 +0100

    Add ft_lst_reverse

---
+++ src/lst/ft_lst_reverse.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   ft_lst_reverse.c                                   :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: sbos <sbos@student.codam.nl>                 +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2021/11/30 15:06:42 by sbos          #+#    #+#                 */
+/*   Updated: 2021/11/30 15:12:45 by sbos          ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft_lst.h"
+
+t_list	*ft_lst_reverse(t_list **lst)
+{
+	t_list	*cur;
+	t_list	*prev;
+	t_list	*next;
+
+	cur = *lst;
+	prev = NULL;
+	while (cur != NULL)
+	{
+		next = cur->next;
+		cur->next = prev;
+		prev = cur;
+		cur = next;
+	}
+	*lst = prev;
+	return (*lst);
+}
