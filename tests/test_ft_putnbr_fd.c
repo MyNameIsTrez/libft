5273e790227a4cdf224b1cb4e34a8933a21e00e6
MyNameIsTrez <welfje@gmail.com>
2022-01-18 17:17:11 +0100

    Add putnbr_fd test

--- tests/test_ft_putnbr_fd.c
+++ tests/test_ft_putnbr_fd.c
@@ -1,6 +1,15 @@
 #include "tests.h"
+#include <fcntl.h>
 
-void	test_ft_putnbr_fd(void)
+Test(ft_putnbr_fd, basic_test)
 {
-	ft_putnbr_fd(-123, 1);
+	int fd = open("/tmp/ft_putnbr_fd_test", O_RDWR | O_CREAT, 0640);
+
+	ft_putnbr_fd(-123, fd);
+	char buf[20];
+	ft_memset(buf, '\0', 20);
+	lseek(fd, 0, SEEK_SET);
+	read(fd, buf, 20);
+	close(fd);
+	cr_assert_str_eq(buf, "-123");
 }
