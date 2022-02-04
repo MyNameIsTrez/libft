e714783a2ab543c2e0ed6ac60fd1f527668db0eb
MyNameIsTrez <welfje@gmail.com>
2022-02-04 16:30:54 +0100

    Update ft_atoi() documentation

--- src/nbr/ft_atoi.c
+++ src/nbr/ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: sbos <sbos@student.codam.nl>                 +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/10/11 12:41:20 by sbos          #+#    #+#                 */
-/*   Updated: 2022/02/04 16:23:58 by sbos          ########   odam.nl         */
+/*   Updated: 2022/02/04 16:30:25 by sbos          ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,14 +33,9 @@ static long	get_number_without_minus(const char *str)
 }
 
 /**
- * @brief Converts @p str to a signed int.\n
-   Its `sign * get_positive_integer(str)` line overflows twice:\n
-   1. What get_positive_integer(str) returns has overflowed.
-   2. Multiplying -1 with the minimum value overflows again.
-   3. The overflowed value is returned.
- *
+ * @brief Converts @p str to a signed int.
  * @param str May start with whitespace, and the first encountered number will
-   be converted.
+ * be converted.
  * @return The converted value or 0 on error.
  */
 int	ft_atoi(const char *str)
