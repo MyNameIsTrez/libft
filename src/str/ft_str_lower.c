cc38e1ff5d67541f6fc261f36eba5b3613340184
MyNameIsTrez <welfje@gmail.com>
2022-03-04 17:37:15 +0100

    Make ft_str_lower() use ft_striteri()

--- src/str/ft_str_lower.c
+++ src/str/ft_str_lower.c
@@ -6,11 +6,21 @@
 /*   By: sbos <sbos@student.codam.nl>                 +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/02/04 18:34:49 by sbos          #+#    #+#                 */
-/*   Updated: 2022/02/05 15:01:01 by sbos          ########   odam.nl         */
+/*   Updated: 2022/03/04 17:36:48 by sbos          ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
+////////////////////////////////////////////////////////////////////////////////
+
 #include "libft.h"
+
+////////////////////////////////////////////////////////////////////////////////
+
+static void	striteri_tolower(unsigned int i, char *chr)
+{
+	(void)i;
+	(*chr) = (char)ft_tolower(*chr);
+}
 
 /**
  * @brief Lowercases all alphabetical characters in @p str.
@@ -23,10 +33,8 @@ char	*ft_str_lower(char *str)
 	int	i;
 
 	i = 0;
-	while (str[i] != '\0')
-	{
-		str[i] = (char)ft_tolower(str[i]);
-		i++;
-	}
+	ft_striteri(str, striteri_tolower);
 	return (str);
 }
+
+////////////////////////////////////////////////////////////////////////////////
