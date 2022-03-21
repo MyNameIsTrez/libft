bcdb229b07293f780a16cb0aaf4d5187689491ca
MyNameIsTrez <welfje@gmail.com>
2022-03-21 13:17:30 +0100

    Add atoi tests

--- tests/nbr/test_ft_atoi.c
+++ tests/nbr/test_ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: sbos <sbos@student.codam.nl>                 +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/02/16 14:30:21 by sbos          #+#    #+#                 */
-/*   Updated: 2022/02/22 14:33:52 by sbos          ########   odam.nl         */
+/*   Updated: 2022/03/21 13:16:54 by sbos          ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,9 +18,64 @@
 
 Test(ft_atoi)
 {
-	massert(ft_atoi(" 12345 "), atoi(" 12345 "));
-	massert(ft_atoi("- 12345"), atoi("- 12345"));
-	massert(ft_atoi("+ 12345"), atoi("+ 12345"));
+	{
+		massert(ft_atoi("-12345"), -12345);
+		massert(ft_atoi("-12345"), atoi("-12345"));
+
+		massert(ft_atoi("+12345"), 12345);
+		massert(ft_atoi("+12345"), atoi("+12345"));
+
+		massert(ft_atoi(".12345"), 0);
+		massert(ft_atoi(".12345"), atoi(".12345"));
+
+		massert(ft_atoi("a12345"), 0);
+		massert(ft_atoi("a12345"), atoi("a12345"));
+
+		massert(ft_atoi(" 12345 "), 12345);
+		massert(ft_atoi(" 12345 "), atoi(" 12345 "));
+	}
+
+	{
+		massert(ft_atoi("- 12345"), 0);
+		massert(ft_atoi("- 12345"), atoi("- 12345"));
+
+		massert(ft_atoi("+ 12345"), 0);
+		massert(ft_atoi("+ 12345"), atoi("+ 12345"));
+
+		massert(ft_atoi(". 12345"), 0);
+		massert(ft_atoi(". 12345"), atoi(". 12345"));
+
+		massert(ft_atoi("a 12345"), 0);
+		massert(ft_atoi("a 12345"), atoi(". 12345"));
+	}
+
+	{
+		massert(ft_atoi("123-45"), 123);
+		massert(ft_atoi("123-45"), atoi("123-45"));
+
+		massert(ft_atoi("123+45"), 123);
+		massert(ft_atoi("123+45"), atoi("123+45"));
+
+		massert(ft_atoi("123.45"), 123);
+		massert(ft_atoi("123.45"), atoi("123.45"));
+
+		massert(ft_atoi("123a45"), 123);
+		massert(ft_atoi("123a45"), atoi("123a45"));
+	}
+
+	{
+		massert(ft_atoi("12345-"), 12345);
+		massert(ft_atoi("12345-"), atoi("12345-"));
+
+		massert(ft_atoi("12345+"), 12345);
+		massert(ft_atoi("12345+"), atoi("12345+"));
+
+		massert(ft_atoi("12345."), 12345);
+		massert(ft_atoi("12345."), atoi("12345."));
+
+		massert(ft_atoi("12345a"), 12345);
+		massert(ft_atoi("12345a"), atoi("12345a"));
+	}
 }
 
 ////////////////////////////////////////////////////////////////////////////////
