318db8ee850776126e9a710a9d97cf9609bf389d
MyNameIsTrez <sbos@student.codam.nl>
2022-06-23 11:13:39 +0200

    Add leak calls.

--- src/file/ft_read_grid_from_file.c
+++ src/file/ft_read_grid_from_file.c
@@ -6,7 +6,7 @@
 /*   By: sbos <sbos@student.codam.nl>                 +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/06/17 16:53:49 by sbos          #+#    #+#                 */
-/*   Updated: 2022/06/22 18:11:06 by sbos          ########   odam.nl         */
+/*   Updated: 2022/06/23 11:13:13 by sbos          ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,6 +29,7 @@ STATIC void	clear_leftover_gnl_lines(int fd)
 
 STATIC t_success	transfer_lst_to_array(t_list *lst, char ***cells)
 {
+	system("leaks tester -q");
 	*cells = (char **)ft_lst_to_array(lst);
 	if (*cells == NULL)
 		return (ERROR);
@@ -51,21 +52,28 @@ t_success	ft_read_grid_from_file(t_grid *grid, char *filename)
 
 	if (fd < 0)
 		return (ERROR);
+	system("leaks tester -q");
 	lst = NULL;
 	if (read_into_lst(grid, fd, &lst) != SUCCESS)
 	{
 		close(fd);
+		system("leaks tester -q");
 		clear_leftover_gnl_lines(fd);
+		system("leaks tester -q");
 		return (ERROR);
 	}
 	close(fd);
+	system("leaks tester -q");
 	ft_lst_reverse(&lst);
+	system("leaks tester -q");
 	if (transfer_lst_to_array(lst, &grid->cells) != SUCCESS)
 	{
 		ft_lstclear(&lst, &free);
 		return (ERROR);
 	}
+	system("leaks tester -q");
 	ft_lstclear(&lst, NULL);
+	system("leaks tester -q");
 	return (SUCCESS);
 }
 
--- src/file/ft_read_grid_from_file_utils.c
+++ src/file/ft_read_grid_from_file_utils.c
@@ -6,7 +6,7 @@
 /*   By: sbos <sbos@student.codam.nl>                 +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/06/22 15:46:48 by sbos          #+#    #+#                 */
-/*   Updated: 2022/06/22 18:23:29 by sbos          ########   odam.nl         */
+/*   Updated: 2022/06/23 11:12:58 by sbos          ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,12 +21,14 @@ STATIC char	*get_next_line_without_newline(int fd)
 	char	*line;
 	char	*nl_ptr;
 
+	system("leaks tester -q");
 	line = get_next_line(fd);
 	if (line == NULL)
 		return (NULL);
 	nl_ptr = ft_strchr(line, '\n');
 	if (nl_ptr != NULL)
 		*nl_ptr = '\0';
+	system("leaks tester -q");
 	return (line);
 }
 
@@ -37,6 +39,7 @@ STATIC t_success	helper_read_into_lst(t_grid *grid, int fd, char *line,
 	grid->height = 0;
 	if (line != NULL)
 		grid->width = ft_strlen(line);
+	system("leaks tester -q");
 	while (line != NULL)
 	{
 		grid->height++;
@@ -50,6 +53,7 @@ STATIC t_success	helper_read_into_lst(t_grid *grid, int fd, char *line,
 			return (ERROR);
 		}
 	}
+	system("leaks tester -q");
 	return (SUCCESS);
 }
 
@@ -57,6 +61,7 @@ t_success	read_into_lst(t_grid *grid, int fd, t_list **lst_ptr)
 {
 	char	*line;
 
+	system("leaks tester -q");
 	line = get_next_line_without_newline(fd);
 	if (ft_lst_new_front(lst_ptr, line) == NULL)
 	{
@@ -64,11 +69,13 @@ t_success	read_into_lst(t_grid *grid, int fd, t_list **lst_ptr)
 		ft_free(&line);
 		return (ERROR);
 	}
+	system("leaks tester -q");
 	if (helper_read_into_lst(grid, fd, line, lst_ptr) != SUCCESS)
 	{
 		ft_lstclear(lst_ptr, &free);
 		return (ERROR);
 	}
+	system("leaks tester -q");
 	return (SUCCESS);
 }
 
