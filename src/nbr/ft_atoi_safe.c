33066a28f16f77e51afad3ee3bcfec698c08b69a
MyNameIsTrez <sbos@student.codam.nl>
2022-06-23 12:56:34 +0200

    Make ft_atoi_safe() return t_success instead of a bool.

--- src/nbr/ft_atoi_safe.c
+++ src/nbr/ft_atoi_safe.c
@@ -6,7 +6,7 @@
 /*   By: sbos <sbos@student.codam.nl>                 +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/10/11 12:41:20 by sbos          #+#    #+#                 */
-/*   Updated: 2022/03/31 17:56:07 by sbos          ########   odam.nl         */
+/*   Updated: 2022/06/23 12:47:28 by sbos          ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,7 +30,7 @@
  * The dereferenced value won't be touched if no number was found in the string.
  * @return true if a number was found in @p str, false otherwise.
  */
-bool	ft_atoi_safe(const char *str, int *nbr)
+t_success	ft_atoi_safe(const char *str, int *nbr)
 {
 	long	sign;
 	bool	out_of_range;
@@ -48,9 +48,11 @@ bool	ft_atoi_safe(const char *str, int *nbr)
 	else if (str[i] == '+')
 		i++;
 	if (NOT ft_isdigit(str[i]))
-		return (false);
+		return (ERROR);
 	*nbr = ft_atoi_range(str, &out_of_range);
-	return (NOT out_of_range);
+	if (out_of_range)
+		return (ERROR);
+	return (SUCCESS);
 }
 
 ////////////////////////////////////////////////////////////////////////////////
--- src/nbr/libft_nbr.h
+++ src/nbr/libft_nbr.h
@@ -6,7 +6,7 @@
 /*   By: sbos <sbos@student.codam.nl>                 +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/11/25 17:27:42 by sbos          #+#    #+#                 */
-/*   Updated: 2022/06/17 17:08:52 by sbos          ########   odam.nl         */
+/*   Updated: 2022/06/23 12:46:11 by sbos          ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,19 +21,19 @@
 
 ////////////////////////////////////////////////////////////////////////////////
 
-int		ft_atoi_range(const char *str, bool *out_of_range);
-bool	ft_atoi_safe(const char *str, int *nbr);
-int		ft_atoi(const char *str);
-char	ft_digit_to_char_base(int nbr, unsigned int base);
-char	ft_digit_to_char(int nbr);
-size_t	ft_get_digit_count_base_unsigned(uintmax_t nbr, unsigned int base);
-size_t	ft_get_digit_count_base(intmax_t nbr, unsigned int base);
-size_t	ft_get_digit_count(intmax_t nbr);
-char	*ft_itoa(int nbr);
-int		ft_max(const int a, const int b);
-int		ft_min(const int a, const int b);
-char	*ft_nbr_to_str(intmax_t number, unsigned int base);
-char	*ft_unsigned_nbr_to_str(uintmax_t number, unsigned int base);
+int			ft_atoi_range(const char *str, bool *out_of_range);
+t_success	ft_atoi_safe(const char *str, int *nbr);
+int			ft_atoi(const char *str);
+char		ft_digit_to_char_base(int nbr, unsigned int base);
+char		ft_digit_to_char(int nbr);
+size_t		ft_get_digit_count_base_unsigned(uintmax_t nbr, unsigned int base);
+size_t		ft_get_digit_count_base(intmax_t nbr, unsigned int base);
+size_t		ft_get_digit_count(intmax_t nbr);
+char		*ft_itoa(int nbr);
+int			ft_max(const int a, const int b);
+int			ft_min(const int a, const int b);
+char		*ft_nbr_to_str(intmax_t number, unsigned int base);
+char		*ft_unsigned_nbr_to_str(uintmax_t number, unsigned int base);
 
 ////////////////////////////////////////////////////////////////////////////////
 
