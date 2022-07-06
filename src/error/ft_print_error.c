edbb7d8a0e10acac72b7bd5985ae37f14b2cd0e5
MyNameIsTrez <sbos@student.codam.nl>
2022-07-06 15:31:03 +0200

    Use enums for lookup table as indices.

--- src/error/ft_print_error.c
+++ src/error/ft_print_error.c
@@ -6,7 +6,7 @@
 /*   By: sbos <sbos@student.codam.nl>                 +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/07/06 13:20:27 by sbos          #+#    #+#                 */
-/*   Updated: 2022/07/06 15:09:39 by sbos          ########   odam.nl         */
+/*   Updated: 2022/07/06 15:30:42 by sbos          ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,15 +20,15 @@
 void	ft_print_error(void)
 {
 	char*const	ft_error_messages[] = {
-		"Invalid file descriptor.",
-		"Malloc failure.",
-		"The input map is not rectangular.",
-		"Too few program arguments were provided.",
-		"Too many program arguments were provided.",
+	[FT_ERROR_INVALID_FD] = "Invalid file descriptor.",
+	[FT_ERROR_MALLOC] = "Malloc failure.",
+	[FT_ERROR_NON_RECTANGULAR_GRID] = "The input map is not rectangular.",
+	[FT_ERROR_TOO_FEW_ARGS] = "Too few program arguments were provided.",
+	[FT_ERROR_TOO_MANY_ARGS] = "Too many program arguments were provided.",
 	};
 
 	ft_print("Error: ");
-	ft_putendl(ft_error_messages[ft_get_error() + 1]);
+	ft_putendl(ft_error_messages[ft_get_error()]);
 }
 
 ////////////////////////////////////////////////////////////////////////////////
