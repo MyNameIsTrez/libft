4f266d258edecdd8b3a4a240ade20ecbe597199a
MyNameIsTrez <sbos@student.codam.nl>
2022-07-25 11:13:25 +0200

    Remove ft_ prefix from helper headers

--- helper_headers/helper_headers.h
+++ helper_headers/helper_headers.h
@@ -1,27 +1,27 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        ::::::::            */
-/*   ft_helper_headers.h                                :+:    :+:            */
+/*   helper_headers.h                                   :+:    :+:            */
 /*                                                     +:+                    */
 /*   By: sbos <sbos@student.codam.nl>                 +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/07/14 14:09:20 by sbos          #+#    #+#                 */
-/*   Updated: 2022/07/22 14:34:52 by sbos          ########   odam.nl         */
+/*   Updated: 2022/07/25 11:12:14 by sbos          ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 ////////////////////////////////////////////////////////////////////////////////
 
-#ifndef FT_HELPER_HEADERS_H
-# define FT_HELPER_HEADERS_H
+#ifndef HELPER_HEADERS_H
+# define HELPER_HEADERS_H
 
 ////////////////////////////////////////////////////////////////////////////////
 
 # include "helper_headers/boolean_operators.h"
 # include "helper_headers/shared_system_headers.h"
-# include "helper_headers/ft_types.h"
-# include "helper_headers/ft_iterators.h"
-# include "helper_headers/ft_open_fd_max.h"
+# include "helper_headers/types.h"
+# include "helper_headers/iterators.h"
+# include "helper_headers/open_fd_max.h"
 # include "helper_headers/whitespace.h"
 
 ////////////////////////////////////////////////////////////////////////////////
