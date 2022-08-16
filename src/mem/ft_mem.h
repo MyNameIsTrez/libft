728afc500156845f92c291e9cfc1c111f79748ba
MyNameIsTrez <sbos@student.codam.nl>
2022-08-16 17:13:00 +0200

    Add ft_memset_struct()

--- src/mem/ft_mem.h
+++ src/mem/ft_mem.h
@@ -6,7 +6,7 @@
 /*   By: sbos <sbos@student.codam.nl>                 +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/11/25 17:26:21 by sbos          #+#    #+#                 */
-/*   Updated: 2022/08/15 15:16:16 by sbos          ########   odam.nl         */
+/*   Updated: 2022/08/16 17:03:58 by sbos          ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,6 +26,8 @@ void	*ft_memchr(const void *ptr, t_i32 chr, size_t len);
 t_i32	ft_memcmp(const void *src1, const void *src2, size_t len);
 void	*ft_memcpy(void *dst, const void *src, size_t len);
 void	*ft_memmove(void *dst, const void *src, size_t len);
+void	*ft_memset_struct(void *dst, void *struct_, size_t struct_size,
+			size_t dst_length);
 void	*ft_memset(void *dst, t_i32 chr, size_t len);
 ssize_t	ft_write(int fildes, const void *buf, size_t nbyte);
 
--- src/mem/ft_memset_struct.c
+++ src/mem/ft_memset_struct.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        ::::::::            */
-/*   ft_memset.c                                        :+:    :+:            */
+/*   ft_memset_struct.c                                 :+:    :+:            */
 /*                                                     +:+                    */
 /*   By: sbos <sbos@student.codam.nl>                 +#+                     */
 /*                                                   +#+                      */
-/*   Created: 2021/10/07 14:24:17 by sbos          #+#    #+#                 */
-/*   Updated: 2022/08/16 17:01:39 by sbos          ########   odam.nl         */
+/*   Created: 2022/08/16 16:55:49 by sbos          #+#    #+#                 */
+/*   Updated: 2022/08/16 17:07:24 by sbos          ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,21 +17,34 @@
 ////////////////////////////////////////////////////////////////////////////////
 
 /**
- * @brief Sets @p len bytes of memory to byte @p chr.
+ * @brief
  *
  * @param dst
- * @param chr
- * @param len
- * @return The passed dst.
+ * @param struct_size
+ * @param dst_length
+ * @return
  */
-void	*ft_memset(void *dst, t_i32 chr, size_t len)
+
+/**
+ * @brief Fills @p dst with copies of struct @p struct_.
+ *
+ * @param dst
+ * @param struc
+ * @param struct_size
+ * @param dst_length
+ * @return
+ */
+void	*ft_memset_struct(void *dst, void *struct_, size_t struct_size,
+			size_t dst_length)
 {
 	size_t	i;
+	size_t	struct_count;
 
 	i = 0;
-	while (i < len)
+	struct_count = dst_length / struct_size;
+	while (i < struct_count)
 	{
-		((t_u8 *)dst)[i] = (t_u8)chr;
+		ft_memcpy(&dst[i * struct_size], struct_, struct_size);
 		i++;
 	}
 	return (dst);
