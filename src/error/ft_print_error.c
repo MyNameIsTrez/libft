cb46a571a6f68ca7afd9875cd0a3cc6ac5f344ae
MyNameIsTrez <sbos@student.codam.nl>
2022-08-26 16:10:26 +0200

    Add error message

--- src/error/ft_print_error.c
+++ src/error/ft_print_error.c
@@ -6,7 +6,7 @@
 /*   By: sbos <sbos@student.codam.nl>                 +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/07/06 13:20:27 by sbos          #+#    #+#                 */
-/*   Updated: 2022/08/11 12:51:14 by sbos          ########   odam.nl         */
+/*   Updated: 2022/08/26 16:09:55 by sbos          ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,12 +19,12 @@
 // TODO: Add tests.
 void	ft_print_error(void)
 {
-	// TODO: Make sure all error enums have an error message
 	char*const	ft_error_messages[] = {
 	[FT_ERROR_INVALID_FD] = "Invalid file descriptor.",
 	[FT_ERROR_READ] = "Read failure.",
 	[FT_ERROR_MALLOC] = "Malloc failure.",
 	[FT_ERROR_NON_RECTANGULAR_GRID] = "The input map is not rectangular.",
+	[FT_ERROR_EMPTY_FILE] = "Tried reading from an empty file.",
 	[FT_ERROR_TOO_FEW_ARGS] = "Too few program arguments were provided.",
 	[FT_ERROR_TOO_MANY_ARGS] = "Too many program arguments were provided.",
 	[FT_ERROR_VECTOR_NOT_FOUND] = "Vector not found in vector metadata.",
